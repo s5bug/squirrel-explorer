0febfec18f15bb87feca207c24b07833bb718f4b
Aly <me@s5.pm>
2024-11-05 22:38:32 -0800

    Remove extra lines on LocalVarInfo

--- scalajs/src/main/scala/tf/bug/cnut/SqLocalVarInfo.scala
+++ scalajs/src/main/scala/tf/bug/cnut/SqLocalVarInfo.scala
@@ -9,14 +9,10 @@ case class SqLocalVarInfo(
 
   def renderInto(indent: Int, renderedCnut: MutableCnutRender): Unit = {
     renderedCnut.fragment("LocalVarInfo(")
-    renderedCnut.line()
-    renderedCnut.space(2 + indent)
     renderedCnut.renderFieldInline("name", 2 + indent, () => renderedCnut.fragment(name.show))
     renderedCnut.renderFieldInline("pos", 2 + indent, () => renderedCnut.fragment(pos.toString))
     renderedCnut.renderFieldInline("startOp", 2 + indent, () => renderedCnut.fragment(startOp.toString))
     renderedCnut.renderFieldInlineLast("endOp", 2 + indent, () => renderedCnut.fragment(endOp.toString))
-    renderedCnut.line()
-    renderedCnut.space(indent)
     renderedCnut.fragment(")")
   }
 
